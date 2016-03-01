d5a2e49fe771e6ede914cd0ef436e8ac528dc6b7
Grant Ramsay <seapagan@gmail.com>
2016-03-01 14:49:49 +0000

    spec/spec_helper.rb : Add some more helpers
    Add helpers for :
    
    SimpleCov
    Coveralls
    PullReview
    should_not
    FakeFS (in case needed)
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,20 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'should_not/rspec'
+require 'simplecov'
+require 'pullreview/coverage'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter,
+  PullReview::Coverage::Formatter
+]
+SimpleCov.start
+
+require 'pp' # work around https://github.com/defunkt/fakefs/issues/99
 require 'update_repo'
+require 'fakefs/spec_helpers'
+
+RSpec.configure do |config|
+  config.include FakeFS::SpecHelpers, fakefs: true
+end
