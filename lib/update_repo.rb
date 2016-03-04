1bd8ba1825d154b6783b451693a238d6aa4e81f9
Grant Ramsay <seapagan@gmail.com>
2016-03-04 13:46:41 +0000

    Refactoring in progress
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lib/update_repo.rb
+++ lib/update_repo.rb
@@ -3,16 +3,21 @@ require 'yaml'
 require 'colorize'
 require 'update_repo/version'
 
+# Overall module with classes performing the functionality
+# Contains Class UpdateRepo::WalkRepo
 module UpdateRepo
+  CONFIG_FILE = '.updatereporc'.freeze
 
-  CONFIG_FILE = '.updatereporc'
-
+  # An encapsulated class to walk the repo directories and update all Git
+  # repositories found therein.
   class WalkRepo
     attr_reader :counter
-  # $counter = 0
+    attr_writer :start_time
+    # $counter = 0
 
     def initialize
       @counter = 0
+      @start_time = 0
     end
 
     def recurse_dir(dirname, exceptions)
@@ -25,10 +30,7 @@ module UpdateRepo
               update_repo(dirpath)
               @counter += 1
             else
-              Dir.chdir(gitpath) do
-                repo_url = `git config remote.origin.url`.chomp
-                print "* Skipping #{dirpath}".yellow, " (#{repo_url})\n"
-              end
+              skip_dir(dirpath)
             end
           else
             recurse_dir(dirpath, exceptions)
@@ -39,25 +41,17 @@ module UpdateRepo
 
     def show_header(configs, location)
       # print an informative header before starting
-      print "\nGit Repo update utility (v", VERSION,")", " \u00A9 Grant Ramsay <seapagan@gmail.com>\n"
+      print "\nGit Repo update utility (v", VERSION, ')',
+            " \u00A9 Grant Ramsay <seapagan@gmail.com>\n"
       puts "Using Configuration from #{location}"
-      puts "\nRepo location(s):".underline
-      configs['location'].each do |loc|
-        print '-> ', loc.cyan, "\n"
-      end
+      list_locations(configs, location)
       if configs['exceptions']
-        print "\nExclusions:".underline, ' ', configs['exceptions'].join(', ').yellow, "\n"
+        print "\nExclusions:".underline, ' ',
+              configs['exceptions'].join(', ').yellow, "\n"
       end
+      # save the start time for later display in the footer...
+      @start_time = Time.now
       puts # blank line before processing starts
-    end
-
-    def update_repo(dirname)
-      Dir.chdir(dirname) do
-        repo_url = `git config remote.origin.url`.chomp
-        print '* ', 'Checking ', dirname.green, " (#{repo_url})\n"
-        print '  -> '
-        system 'git pull'
-      end
     end
 
     def check_config
@@ -77,6 +71,37 @@ module UpdateRepo
       # otherwise return error
       puts "Error : Cannot find configuration file '#{user_config}', aborting".red
       exit 1
+    end
+
+    def footer
+      # print out a brief footer. This will be expanded later.
+      duration = Time.now - @start_time
+      print "\nUpdates completed - #{@counter} repositories processed in ",
+            "#{Time.at(duration).utc.strftime('%H:%M:%S')}\n\n".cyan
+    end
+
+    private
+
+    def list_locations(configs, location)
+      puts "\nRepo location(s):".underline
+      configs['location'].each do |loc|
+        print '-> ', loc.cyan, "\n"
+      end
+    end
+
+    def skip_dir(dirpath)
+      Dir.chdir(dirpath) do
+        repo_url = `git config remote.origin.url`.chomp
+        print "* Skipping #{dirpath}".yellow, " (#{repo_url})\n"
+      end
+    end
+
+    def update_repo(dirname)
+      Dir.chdir(dirname) do
+        repo_url = `git config remote.origin.url`.chomp
+        print '* ', 'Checking ', dirname.green, " (#{repo_url})\n", '  -> '
+        system 'git pull'
+      end
     end
   end
 end
