5e06306859a07cc988791766a92a8f16b55e0baa
Grant Ramsay <seapagan@gmail.com>
2016-03-04 19:12:02 +0000

    Refactor - Moved all methods except 'initialize' and 'start' to private
    Since we now start the process using the 'start' method, no need for others
    to be public any more.
    
    Also update comments for all methods and attributes, so 'inch --pedantic' reports no violations.
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lib/update_repo.rb
+++ lib/update_repo.rb
@@ -6,19 +6,27 @@ require 'update_repo/version'
 # Overall module with classes performing the functionality
 # Contains Class UpdateRepo::WalkRepo
 module UpdateRepo
+  # This constant holds the name to the config file, located in ~/
   CONFIG_FILE = '.updatereporc'.freeze
 
   # An encapsulated class to walk the repo directories and update all Git
   # repositories found therein.
   class WalkRepo
+    # Read-only attribute holding the total number of traversed repositories
+    # @attr_reader :counter [fixnum] total number of traversed repositories
     attr_reader :counter
 
+    # Class constructor. No parameters required.
+    # @return [void]
     def initialize
       @counter = 0
       @start_time = 0
     end
 
-    # This function will perform the required actions
+    # This function will perform the required actions to traverse the Repo.
+    # @example
+    #   walk_repo = UpdateRepo::WalkRepo.new
+    #   walk_repo.start
     def start
       configs, location = check_config
       show_header(configs, location)
@@ -29,6 +37,12 @@ module UpdateRepo
       footer
     end
 
+    private
+
+    # take each directory contained in the Repo directory, and if it is detected
+    # as a Git repository then update it.
+    # @param dirname [string] Contains the directory to search for Git repos.
+    # @param exceptions [array] Each repo matching one of these will be ignored.
     def recurse_dir(dirname, exceptions)
       Dir.foreach(dirname) do |dir|
         dirpath = dirname + '/' + dir
@@ -42,21 +56,10 @@ module UpdateRepo
       end
     end
 
-    def show_header(configs, location)
-      # print an informative header before starting
-      print "\nGit Repo update utility (v", VERSION, ')',
-            " \u00A9 Grant Ramsay <seapagan@gmail.com>\n"
-      print "Using Configuration from #{location}\n"
-      list_locations(configs)
-      if configs['exceptions']
-        print "\nExclusions:".underline, ' ',
-              configs['exceptions'].join(', ').yellow, "\n"
-      end
-      # save the start time for later display in the footer...
-      @start_time = Time.now
-      print "\n" # blank line before processing starts
-    end
-
+    # locate the configuration file and return this data.
+    # Note that this will be re-written later to use my 'confoog' gem.
+    # @return [hash] Hash containing all the configuration parameters.
+    # @return [string] Location of the configuration file.
     def check_config
       # locate the configuration file.
       # first we check in this script location and if present then we ignore any
@@ -77,14 +80,34 @@ module UpdateRepo
       exit 1
     end
 
+    # Display a simple header to the console
+    # @param configs [hash] Configuration hash, used here to list exceptions.
+    # @param location [string] location of config file.
+    # @example
+    #   show_header
+    # @return []
+    def show_header(configs, location)
+      # print an informative header before starting
+      print "\nGit Repo update utility (v", VERSION, ')',
+            " \u00A9 Grant Ramsay <seapagan@gmail.com>\n"
+      print "Using Configuration from #{location}\n"
+      list_locations(configs)
+      if configs['exceptions']
+        print "\nExclusions:".underline, ' ',
+              configs['exceptions'].join(', ').yellow, "\n"
+      end
+      # save the start time for later display in the footer...
+      @start_time = Time.now
+      print "\n" # blank line before processing starts
+    end
+
+    # print out a brief footer. This will be expanded later.
+    # @return [void]
     def footer
-      # print out a brief footer. This will be expanded later.
       duration = Time.now - @start_time
       print "\nUpdates completed - #{@counter} repositories processed in ",
             "#{Time.at(duration).utc.strftime('%H:%M:%S')}\n\n".cyan
     end
-
-    private
 
     def list_locations(configs)
       print "\nRepo location(s):\n".underline
