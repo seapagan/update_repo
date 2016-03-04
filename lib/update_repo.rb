7412599cde2efc2f9a304874012efa55a9c3a68d
Grant Ramsay <seapagan@gmail.com>
2016-03-04 12:16:28 +0000

    Convert base functionality to a class.
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lib/update_repo.rb
+++ lib/update_repo.rb
@@ -1,73 +1,82 @@
 require 'update_repo/version'
 require 'yaml'
 require 'colorize'
+require 'update_repo/version'
 
 module UpdateRepo
 
   CONFIG_FILE = '.updatereporc'
-  $counter = 0
 
-  def recurse_dir(dirname, exceptions)
-    Dir.foreach(dirname) do |dir|
-      dirpath = dirname + '/' + dir
-      if File.directory?(dirpath) && dir != '.' && dir != '..'
-        gitpath = dirpath + '/.git'
-        if File.exist?(gitpath) && File.directory?(gitpath)
-          if !exceptions.include?(dir.chomp)
-            update_repo(dirpath)
-            $counter += 1
-          else
-            Dir.chdir(gitpath) do
-              repo_url = `git config remote.origin.url`.chomp
-              print "* Skipping #{dirpath}".yellow, " (#{repo_url})\n"
+  class WalkRepo
+    attr_reader :counter
+  # $counter = 0
+
+    def initialize
+      @counter = 0
+    end
+
+    def recurse_dir(dirname, exceptions)
+      Dir.foreach(dirname) do |dir|
+        dirpath = dirname + '/' + dir
+        if File.directory?(dirpath) && dir != '.' && dir != '..'
+          gitpath = dirpath + '/.git'
+          if File.exist?(gitpath) && File.directory?(gitpath)
+            if !exceptions.include?(dir.chomp)
+              update_repo(dirpath)
+              @counter += 1
+            else
+              Dir.chdir(gitpath) do
+                repo_url = `git config remote.origin.url`.chomp
+                print "* Skipping #{dirpath}".yellow, " (#{repo_url})\n"
+              end
             end
+          else
+            recurse_dir(dirpath, exceptions)
           end
-        else
-          recurse_dir(dirpath, exceptions)
         end
       end
     end
-  end
 
-  def show_header(configs, location)
-    # print an informative header before starting
-    print "\nGit Repo update utility (v.0.1.0)".underline, " \u00A9 Grant Ramsay <seapagan@gmail.com>\n"
-    puts "Using Configuration from #{location}"
-    puts "\nRepo location(s):".underline
-    configs['location'].each do |loc|
-      print '-> ', loc.cyan, "\n"
+    def show_header(configs, location)
+      # print an informative header before starting
+      print "\nGit Repo update utility (v", VERSION,")", " \u00A9 Grant Ramsay <seapagan@gmail.com>\n"
+      puts "Using Configuration from #{location}"
+      puts "\nRepo location(s):".underline
+      configs['location'].each do |loc|
+        print '-> ', loc.cyan, "\n"
+      end
+      if configs['exceptions']
+        print "\nExclusions:".underline, ' ', configs['exceptions'].join(', ').yellow, "\n"
+      end
+      puts # blank line before processing starts
     end
-    if configs['exceptions']
-      print "\nExclusions:".underline, ' ', configs['exceptions'].join(', ').yellow, "\n"
-    end
-    puts # blank line before processing starts
-  end
 
-  def update_repo(dirname)
-    Dir.chdir(dirname) do
-      repo_url = `git config remote.origin.url`.chomp
-      print '* ', 'Checking ', dirname.green, " (#{repo_url})\n"
-      print '  -> '
-      system 'git pull'
+    def update_repo(dirname)
+      Dir.chdir(dirname) do
+        repo_url = `git config remote.origin.url`.chomp
+        print '* ', 'Checking ', dirname.green, " (#{repo_url})\n"
+        print '  -> '
+        system 'git pull'
+      end
     end
-  end
 
-  def check_config
-    # locate the configuration file.
-    # first we check in this script location and if present then we ignore any
-    # further files.
-    dev_config = File.join '.', CONFIG_FILE
-    user_config = File.join Gem.user_home, CONFIG_FILE
+    def check_config
+      # locate the configuration file.
+      # first we check in this script location and if present then we ignore any
+      # further files.
+      dev_config = File.join '.', CONFIG_FILE
+      user_config = File.join Gem.user_home, CONFIG_FILE
 
-    if File.exist? dev_config
-      # configuration exists in script directory so we ignore any others.
-      return YAML.load_file(dev_config), File.expand_path(dev_config)
-    elsif File.exist? user_config
-      # configuration exists in user home directory so we use that.
-      return YAML.load_file(user_config), File.expand_path(user_config)
+      if File.exist? dev_config
+        # configuration exists in script directory so we ignore any others.
+        return YAML.load_file(dev_config), File.expand_path(dev_config)
+      elsif File.exist? user_config
+        # configuration exists in user home directory so we use that.
+        return YAML.load_file(user_config), File.expand_path(user_config)
+      end
+      # otherwise return error
+      puts "Error : Cannot find configuration file '#{user_config}', aborting".red
+      exit 1
     end
-    # otherwise return error
-    puts "Error : Cannot find configuration file '#{user_config}', aborting".red
-    exit 1
   end
 end
