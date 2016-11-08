7f3291bc2e4c94f08b4d7c81045fb64627682262
Grant Ramsay <seapagan@gmail.com>
2016-11-08 20:01:00 +0000

    Remove threading and consolidate Git output handling.
    The use of peopen3 and threads was leading to undependable results - different
    runs would have differing results dueo to race issues and similar.
    Switched to popen2e which combines stdout and stderr into one stream seems to
    fix this. As a bonus, some of the Git output that had previously been missed is
    now also displayed.
    Also consolidated the output handling into one function 'handle_output' instead
    of splitting the streams.
    This finally makes the issue reported in #24 100% reproducable!
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lib/update_repo/git_control.rb
+++ lib/update_repo/git_control.rb
@@ -31,10 +31,8 @@ module UpdateRepo
     # @return [void]
     def update
       print_log '* Checking ', @dir.green, " (#{repo_url})\n"
-      Open3.popen3("git -C #{@dir} pull") do |stdin, stdout, stderr, thread|
-        stdin.close
-        do_threads(stdout, stderr)
-        thread.join
+      Open3.popen2e("git -C #{@dir} pull") do |stdin, stdout_err, thread|
+        stdout_err.each { |line| handle_output(line) }
       end
       # reset the updated status in the rare case than both update and failed
       # are set. This does happen!
@@ -50,41 +48,21 @@ module UpdateRepo
       `git -C #{@dir} config remote.origin.url`.chomp
     end
 
-    # Create 2 individual threads to handle both STDOUT and STDERR streams,
-    # writing to console and log if specified.
-    # @param stdout [stream] STDOUT Stream from the popen3 call
-    # @param stderr [stream] STDERR Stream from the popen3 call
-    # @return [void]
-    def do_threads(stdout, stderr)
-      { out: stdout, err: stderr }.each do |key, stream|
-        Thread.new do
-          while (line = stream.gets)
-            handle_err(line) if key == :err
-            handle_output(line) if key == :out
-          end
-        end
-      end
-    end
-
-    # output an error line and update the metrics
-    # @param line [string] The string containing the error message
-    # @return [void]
-    def handle_err(line)
-      return unless line =~ /^fatal:|^error:/
-      print_log '   ', line.red
-      @status[:failed] = true
-      err_loc = "#{@dir} (#{repo_url})"
-      @metrics[:failed_list].push(loc: err_loc, line: line)
-    end
-
     # print a git output line and update the metrics if an update has occurred
     # @param line [string] The string containing the git output line
     # @return [void]
     # rubocop:disable Metrics/LineLength
     def handle_output(line)
-      print_log '   ', line.cyan
-      @status[:updated] = true if line =~ /^Updating\s[0-9a-f]{7}\.\.[0-9a-f]{7}/
-      @status[:unchanged] = true if line =~ /^Already up-to-date./
+      if line.chomp =~ /^fatal:|^error:/
+        print_log '   ', line.red
+        @status[:failed] = true
+        err_loc = "#{@dir} (#{repo_url})"
+        @metrics[:failed_list].push(loc: err_loc, line: line)
+      else
+        print_log '   ', line.cyan
+        @status[:updated] = true if line =~ /^Updating\s[0-9a-f]{7}\.\.[0-9a-f]{7}/
+        @status[:unchanged] = true if line =~ /^Already up-to-date./
+      end
     end
     # rubocop:enable Metrics/LineLength
   end
