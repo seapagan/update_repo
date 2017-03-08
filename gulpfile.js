9c86a11e89d6483a5e0ba08185ce2ab1aeea8b24
Grant Ramsay <seapagan@gmail.com>
2017-03-08 14:38:33 +0000

    Web: Add image copy and min to Gulpfile.
    Added ability to copy any images from web/img to docs/image in gulp flow. Currently we
    have no images but this allows for future changes.
    
    Also small re-arrangement to gulpfile so autoprefixer is run after the sass generation.
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,23 @@ var browserify = require('gulp-browserify');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var merge = require('merge-stream');
+var newer = require('gulp-newer');
+var imagemin = require('gulp-imagemin');
 
 var SOURCEPATHS = {
   sassSource : 'web/sass/*.scss',
   htmlSource : 'web/*.html',
   jsSource   : 'web/js/**',
-  cssSource  : 'web/css/**'
+  cssSource  : 'web/css/**',
+  imgSource  : 'web/img/**'
 };
 
 var APPPATH = {
   root  : 'docs/',
   css   : 'docs/css',
   js    : 'docs/js',
-  fonts : 'docs/fonts'
+  fonts : 'docs/fonts',
+  img   : 'docs/img'
 };
 
 gulp.task('clean-html', function() {
@@ -38,8 +42,8 @@ gulp.task('sass', function() {
   var cssFiles = gulp.src([SOURCEPATHS.cssSource, './node_modules/font-awesome/css/font-awesome.css', './node_modules/prismjs/themes/prism.css']);
 
   sassFiles = gulp.src(SOURCEPATHS.sassSource)
-    .pipe(autoprefixer())
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError));
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(autoprefixer());
     return merge(cssFiles, bootstrapCSS, sassFiles)
       .pipe(concat('site.css'))
       .pipe(gulp.dest(APPPATH.css));
@@ -61,6 +65,13 @@ gulp.task('copy', ['clean-html'], function() {
     .pipe(gulp.dest(APPPATH.root));
 });
 
+gulp.task('images', function() {
+  return gulp.src(SOURCEPATHS.imgSource)
+    .pipe(newer(APPPATH.img))
+    .pipe(imagemin())
+    .pipe(gulp.dest(APPPATH.img));
+});
+
 gulp.task('moveFonts', function() {
   gulp.src(['./node_modules/bootstrap/dist/fonts/**', './node_modules/font-awesome/fonts/**'])
     .pipe(gulp.dest(APPPATH.fonts));
@@ -70,11 +81,12 @@ gulp.task('serve', ['sass'], function() {
   browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
     server: {
       baseDir : APPPATH.root
-    }
+    },
+    open: false
   })
 });
 
-gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts', 'moveFonts'], function() {
+gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts', 'moveFonts', 'images'], function() {
   gulp.watch([SOURCEPATHS.sassSource, SOURCEPATHS.cssSource], ['sass']);
   gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
   gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
