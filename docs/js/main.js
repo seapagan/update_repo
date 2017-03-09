a89bd02b9e0712ecd3316df3ffbfe28c2ada5682
Grant Ramsay <seapagan@gmail.com>
2017-03-09 08:04:15 +0000

    Switch to production files
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- docs/js/main.js
+++ docs/js/main.js
@@ -13244,9 +13244,15 @@ global.jQuery = require('jquery');
 bootstrap = require('bootstrap');
 mustache = require('mustache');
 
-// jQuery(document).ready(function() {
-//   alert("hello");
-// });
+jQuery(document).ready(function($) {
+  var jqxhr = $.getJSON('../data.json', function() {
+
+  }).done(function(data) {
+    var template = $('#template').html();
+    var showTemplate = mustache.render(template, data);
+    $('#switches').html(showTemplate);
+  });
+});
 
 // Site-specific Javascript
 
