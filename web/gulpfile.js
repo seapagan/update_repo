e99b8c3c804cc7051fcee5088d1ed3010dc29e6c
Grant Ramsay <seapagan@gmail.com>
2017-06-15 14:33:21 +0100

    Web: Add semicolons back to gulpfile.js. Just because.
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,22 +1,22 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var browserSync = require('browser-sync')
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
 // var reload = browserSync.reload;
-var autoprefixer = require('gulp-autoprefixer')
-var browserify = require('gulp-browserify')
-var clean = require('gulp-clean')
-var concat = require('gulp-concat')
-var merge = require('merge-stream')
-var newer = require('gulp-newer')
-var imagemin = require('gulp-imagemin')
-var injectPartials = require('gulp-inject-partials')
-var minify = require('gulp-minify')
-var cssmin = require('gulp-cssmin')
-var rename = require('gulp-rename')
-var htmlmin = require('gulp-htmlmin')
-var gutil = require('gulp-util')
-var htmlreplace = require('gulp-html-replace')
-var mustache = require('gulp-mustache')
+var autoprefixer = require('gulp-autoprefixer');
+var browserify = require('gulp-browserify');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var merge = require('merge-stream');
+var newer = require('gulp-newer');
+var imagemin = require('gulp-imagemin');
+var injectPartials = require('gulp-inject-partials');
+var minify = require('gulp-minify');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var htmlmin = require('gulp-htmlmin');
+var gutil = require('gulp-util');
+var htmlreplace = require('gulp-html-replace');
+var mustache = require('gulp-mustache');
 
 var SOURCEPATHS = {
   sassSource: 'sass/*.scss',
@@ -27,7 +27,7 @@ var SOURCEPATHS = {
   cssSource: 'css/**',
   imgSource: 'img/**',
   jsonSource: 'webdata.json'
-}
+};
 
 var APPPATH = {
   root: '../docs/',
@@ -35,55 +35,45 @@ var APPPATH = {
   js: '../docs/js',
   fonts: '../docs/fonts',
   img: '../docs/img'
-}
+};
 
 // determine if we want production mode (minified js/css/html) or not
-var isProduction = false
+var isProduction = false;
 if (gutil.env.prod === true) {
-  isProduction = true
+  isProduction = true;
 }
 
 gulp.task('clean-all', function () {
   return gulp.src([APPPATH.root + '/*.html', APPPATH.css + '/*.css', APPPATH.js + '/*.js'], {read: false, force: true})
-    .pipe(clean({force: true}))
-})
-
-// gulp.task('clean-css', function () {
-//   return gulp.src(APPPATH.css + '/*.css', {read: false, force: true})
-//     .pipe(clean({force: true}))
-// })
-
-// gulp.task('clean-scripts', function () {
-//   return gulp.src(APPPATH.js + '/*.js', {read: false, force: true})
-//     .pipe(clean({force: true}))
-// })
+    .pipe(clean({force: true}));
+});
 
 gulp.task('sass', function () {
-  var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css')
-  var sassFiles
-  var cssFiles = gulp.src([SOURCEPATHS.cssSource, './node_modules/font-awesome/css/font-awesome.css', './node_modules/prismjs/themes/prism.css'])
+  var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+  var sassFiles;
+  var cssFiles = gulp.src([SOURCEPATHS.cssSource, './node_modules/font-awesome/css/font-awesome.css', './node_modules/prismjs/themes/prism.css']);
 
   sassFiles = gulp.src(SOURCEPATHS.sassSource)
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(autoprefixer())
+    .pipe(autoprefixer());
   return merge(cssFiles, bootstrapCSS, sassFiles)
     .pipe(concat('site.css'))
     .pipe(isProduction ? cssmin({keepSpecialComments: 0}) : gutil.noop())
     .pipe(isProduction ? rename({suffix: '.min'}) : gutil.noop())
-    .pipe(gulp.dest(APPPATH.css))
-})
+    .pipe(gulp.dest(APPPATH.css));
+});
 
 gulp.task('scripts', function () {
-  var prismJS = './node_modules/prismjs/prism.js'
-  var prismYAML = './node_modules/prismjs/components/prism-yaml.js'
-  var prismWS = './node_modules/prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js'
+  var prismJS = './node_modules/prismjs/prism.js';
+  var prismYAML = './node_modules/prismjs/components/prism-yaml.js';
+  var prismWS = './node_modules/prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js';
 
   gulp.src([SOURCEPATHS.jsSource, prismJS, prismYAML, prismWS])
     .pipe(concat('main.js'))
     .pipe(browserify())
     .pipe(isProduction ? minify({noSource: true}) : gutil.noop())
-    .pipe(gulp.dest(APPPATH.js))
-})
+    .pipe(gulp.dest(APPPATH.js));
+});
 
 gulp.task('html', function () {
   return gulp.src(SOURCEPATHS.htmlSource)
@@ -93,20 +83,20 @@ gulp.task('html', function () {
     .pipe(mustache(SOURCEPATHS.jsonSource))
     .pipe(isProduction ? htmlreplace({'css': 'css/site.min.css', 'js': 'js/main-min.js'}) : gutil.noop())
     .pipe(isProduction ? htmlmin({collapseWhitespace: true, removeComments: true}) : gutil.noop())
-    .pipe(gulp.dest(APPPATH.root))
-})
+    .pipe(gulp.dest(APPPATH.root));
+});
 
 gulp.task('images', function () {
   return gulp.src(SOURCEPATHS.imgSource)
     .pipe(newer(APPPATH.img))
     .pipe(imagemin())
-    .pipe(gulp.dest(APPPATH.img))
-})
+    .pipe(gulp.dest(APPPATH.img));
+});
 
 gulp.task('moveFonts', function () {
   gulp.src(['./node_modules/bootstrap/dist/fonts/**', './node_modules/font-awesome/fonts/**'])
-    .pipe(gulp.dest(APPPATH.fonts))
-})
+    .pipe(gulp.dest(APPPATH.fonts));
+});
 
 gulp.task('serve', ['sass', 'scripts'], function () {
   browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
@@ -114,20 +104,20 @@ gulp.task('serve', ['sass', 'scripts'], function () {
       baseDir: APPPATH.root
     },
     open: false
-  })
-})
+  });
+});
 
 gulp.task('output-env', function () {
-  return isProduction ? gutil.log(gutil.colors.red.bold.underline('Running PRODUCTION environment')) : gutil.log(gutil.colors.green.bold.underline('Running DEVELOPMENT environment'))
-})
+  return isProduction ? gutil.log(gutil.colors.red.bold.underline('Running PRODUCTION environment')) : gutil.log(gutil.colors.green.bold.underline('Running DEVELOPMENT environment'));
+});
 
 gulp.task('watch', ['output-env', 'serve', 'clean-all', 'moveFonts', 'images', 'html'], function () {
-  gulp.watch([SOURCEPATHS.sassSource, SOURCEPATHS.sassPartials, SOURCEPATHS.cssSource], ['sass'])
-  gulp.watch([SOURCEPATHS.jsSource], ['scripts'])
-  gulp.watch([SOURCEPATHS.imgSource], ['images'])
-  gulp.watch([SOURCEPATHS.htmlSource, SOURCEPATHS.htmlPartials, SOURCEPATHS.jsonSource], ['html'])
-})
+  gulp.watch([SOURCEPATHS.sassSource, SOURCEPATHS.sassPartials, SOURCEPATHS.cssSource], ['sass']);
+  gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
+  gulp.watch([SOURCEPATHS.imgSource], ['images']);
+  gulp.watch([SOURCEPATHS.htmlSource, SOURCEPATHS.htmlPartials, SOURCEPATHS.jsonSource], ['html']);
+});
 
-gulp.task('default', ['watch'])
+gulp.task('default', ['watch']);
 
-gulp.task('build', ['output-env', 'sass', 'scripts', 'clean-all', 'moveFonts', 'images', 'html'])
+gulp.task('build', ['output-env', 'sass', 'scripts', 'clean-all', 'moveFonts', 'images', 'html']);
