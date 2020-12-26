084bfc368dd053aec7ec8b5f402929e3e9828baf
Grant Ramsay <seapagan@gmail.com>
2020-12-26 17:25:04 +0000

    Add modular partials

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,5 +1,7 @@
-const { clear } = require("console");
 const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPartialsPlugin = require("html-webpack-partials-plugin");
 
 const postCSSPlugins = [
   require("postcss-import"),
@@ -15,6 +17,42 @@ module.exports = {
     path: path.resolve(__dirname, "../docs"),
   },
   mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title:
+        "update_repo | Automate the update of multiple local Git repository clones",
+    }),
+    new HtmlWebpackPartialsPlugin([
+      {
+        path: path.join(__dirname, "./partials/_links.html"),
+        location: "head",
+      },
+      {
+        path: path.join(__dirname, "partials/_navbar.html"),
+      },
+      {
+        path: path.join(__dirname, "partials/_about.html"),
+      },
+      {
+        path: path.join(__dirname, "partials/_installation.html"),
+      },
+      {
+        path: path.join(__dirname, "partials/_usage.html"),
+      },
+      {
+        path: path.join(__dirname, "partials/_configuration.html"),
+      },
+      {
+        path: path.join(__dirname, "partials/_contribute.html"),
+      },
+      {
+        path: path.join(__dirname, "partials/_license.html"),
+      },
+      {
+        path: path.join(__dirname, "partials/_footer.html"),
+      },
+    ]),
+  ],
   watch: true,
   module: {
     rules: [
