5a8777129f8ff35cd1da4a49b97caa0f4cd546eb
Grant Ramsay <seapagan@gmail.com>
2020-12-29 15:26:16 +0000

    fix functionality to read latest live gem version

--- web/assets/scripts/Main.js
+++ web/assets/scripts/Main.js
@@ -4,3 +4,15 @@ import "../styles/styles.css";
 if (module.hot) {
   module.hot.accept();
 }
+
+// return the version number of the latest released Gem
+function getgemver() {
+  // URL to the API...
+  var APIurl = "https://rubygems.org/api/v1/gems/update_repo.json";
+  jQuery.getJSON(APIurl, function (data) {
+    jQuery("#version").text(data.version);
+  });
+}
+
+// call the function to get the latest our published gem version
+getgemver();
