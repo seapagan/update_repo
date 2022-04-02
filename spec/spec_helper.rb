691d7b9b87e56b82cb070dc9faddfdd0072d37fe
Grant Ramsay <seapagan@gmail.com>
2022-04-02 20:19:00 +0000

    hopefully get coveralls working again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,20 @@
 # frozen_string_literal: true
 
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'should_not/rspec'
 require 'simplecov'
 require 'pullreview/coverage'
 # require 'coveralls'
 
-SimpleCov.formatters = [
-  SimpleCov::Formatter::HTMLFormatter,
-  # Coveralls::SimpleCov::Formatter,
-  PullReview::Coverage::Formatter
-]
-SimpleCov.start
+# SimpleCov.formatters = [
+#   # SimpleCov::Formatter::HTMLFormatter,
+#   # Coveralls::SimpleCov::Formatter,
+#   # PullReview::Coverage::Formatter
+# ]
+# SimpleCov.start
 
 require 'pp' # work around https://github.com/defunkt/fakefs/issues/99
 require 'update_repo'
--- update_repo.gemspec
+++ update_repo.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_development_dependency 'bundler'
-  # spec.add_development_dependency 'coveralls'
+  spec.add_development_dependency 'coveralls'
   spec.add_development_dependency 'fakefs'
   spec.add_development_dependency 'inch'
   spec.add_development_dependency 'pry'
